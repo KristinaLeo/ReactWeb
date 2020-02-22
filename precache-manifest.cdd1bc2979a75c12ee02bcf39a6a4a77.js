9f79d15d70734158157337184eac630a2cc56361
KristinaLeo <kristina.leo.15@gmail.com>
2020-02-21 22:26:05 -0800

    Updates

---
+++ precache-manifest.cdd1bc2979a75c12ee02bcf39a6a4a77.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d58bab99d7a8c2908fe593cb9e83d7",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "a53d3551d889ce11c2bb",
+    "url": "/reactweb/static/css/main.78cf9865.chunk.css"
+  },
+  {
+    "revision": "f4f4751f28c244098c4e",
+    "url": "/reactweb/static/js/2.c6b9cc5b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactweb/static/js/2.c6b9cc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53d3551d889ce11c2bb",
+    "url": "/reactweb/static/js/main.dae83baf.chunk.js"
+  },
+  {
+    "revision": "909877b510ac1d7c1755",
+    "url": "/reactweb/static/js/runtime-main.11caa90b.js"
+  },
+  {
+    "revision": "bcd7793f6cdc06b290133bc58daad389",
+    "url": "/reactweb/static/media/about.bcd7793f.png"
+  },
+  {
+    "revision": "faf81c8ca3de305dcea2f35488f5bdef",
+    "url": "/reactweb/static/media/avatar.faf81c8c.png"
+  },
+  {
+    "revision": "17b89299d50d486b4d65a47b7ce4ee38",
+    "url": "/reactweb/static/media/boba.17b89299.png"
+  },
+  {
+    "revision": "c2c6860ea50118bc5241eea32d0bb3b2",
+    "url": "/reactweb/static/media/disney1.c2c6860e.png"
+  },
+  {
+    "revision": "b3973f5f678eb64d7b1bfeca70a44061",
+    "url": "/reactweb/static/media/htmlweb.b3973f5f.png"
+  },
+  {
+    "revision": "40617aa59625c70476c95eb4f74f5a05",
+    "url": "/reactweb/static/media/leaves.40617aa5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81cc803f314a78fd524ae764c0842348",
+    "url": "/reactweb/static/media/pethub.81cc803f.png"
+  }
+]);
